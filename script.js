0a4295d4144fae09ee19ca44f049c7e12f7b3fbd
Nick Sanchez <nicksanchez2371@gmail.com>
2024-02-14 17:01:31 -0600

    navbar is finished

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menuLinks');
+    const Icon = document.querySelector('.hamburgerIcon');
+    menu.classList.toggle('open');
+    Icon.classList.toggle('open');
+
+}
